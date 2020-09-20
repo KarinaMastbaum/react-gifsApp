f0d11d26da80a853100dacab56906f1b0d6ffbff
karina.mastbaum@gmail.com <Mona Lisa>
2020-09-20 14:41:27 -0300

    docs agregado

---
+++ docs/precache-manifest.947dfe61d8b48dd0e8d9c5c644e31962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf8ec617398eb872f901f0262f65939",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c520a73be74999b872fb",
+    "url": "/static/css/main.79e487b7.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c520a73be74999b872fb",
+    "url": "/static/js/main.96807f2e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
